2e637d77a64c290927f1b328d363bc19e3007a64
Lygia Rodrigues <lygiardrgs@gmail.com>
2024-03-07 22:12:03 -0300

    Feat: Adição da página Histórico Acadêmico estruturada em JavaScript.

---
+++ historico.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    $('.login-info-box').fadeOut();
+    $('.login-show').addClass('show-log-panel');
+});
+
+$('.login-reg-panel input[type="radio"]').on('change', function() {
+    if($('#log-login-show').is(':checked')) {
+        $('.register-info-box').fadeOut(); 
+        $('.login-info-box').fadeIn();
+        
+        $('.white-panel').addClass('right-log');
+        $('.register-show').addClass('show-log-panel');
+        $('.login-show').removeClass('show-log-panel');
+    }
+    else if($('#log-reg-show').is(':checked')) {
+        $('.register-info-box').fadeIn();
+        $('.login-info-box').fadeOut();
+        
+        $('.white-panel').removeClass('right-log');
+        
+        $('.login-show').addClass('show-log-panel');
+        $('.register-show').removeClass('show-log-panel');
+    }
+});
+
+$("#cpfcnpj").keydown(function(){
+    try {
+        $("#cpfcnpj").unmask();
+    } catch (e) {}
+
+    var tamanho = $("#cpfcnpj").val().length;
+
+    if(tamanho < 11){
+        $("#cpfcnpj").mask("999.999.999-99");
+    } else {
+        $("#cpfcnpj").mask("99.999.999/9999-99");
+    }
+
+    // ajustando foco
+    var elem = this;
+    setTimeout(function(){
+        // mudo a posição do seletor
+        elem.selectionStart = elem.selectionEnd = 10000;
+    }, 0);
+    // reaplico o valor para mudar o foco
+    var currentValue = $(this).val();
+    $(this).val('');
+    $(this).val(currentValue);
+});
+
+// Validação do formulário
+function Validar(){
+    var email = document.getElementById('email');
+    var cpfcnpj = document.getElementById('cpfcnpj');
+    var senha = document.getElementById('senha');
+    var senhaconfirm = document.getElementById('senhaconfirm');
+
+    if(email.value == ""){
+        alert("Campo EMAIL vazio, favor verificar!");
+        email.focus();
+    }
+}
